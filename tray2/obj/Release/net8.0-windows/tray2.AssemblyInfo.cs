39e767981ad9d1aab0a95cc2080ec65b2e71505c
ShatunovNA <nik.al.shatunov@gmail.com>
2025-01-31 17:27:13 +0500

    zip

--- tray2/obj/Release/net8.0-windows/tray2.AssemblyInfo.cs
+++ tray2/obj/Release/net8.0-windows/tray2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("tray2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39fc8d8c1bcb8ff4bf3a0a5aeb44ebeaa80e6052")]
 [assembly: System.Reflection.AssemblyProductAttribute("tray2")]
